abdcb3991e7589cb9ade4f59905d1c9e9971383b
coreylad <32624703+coreylad@users.noreply.github.com>
2026-02-25 18:34:02 +0000

    Add MigrationPanel Livewire component
    Introduce a basic Livewire component stub at app/Http/Livewire/MigrationPanel.php. The component implements render() returning view('livewire.migration-panel') to serve as the UI entry point for a migration panel.

---
+++ app/Http/Livewire/MigrationPanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MigrationPanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.migration-panel');
+    }
+}
